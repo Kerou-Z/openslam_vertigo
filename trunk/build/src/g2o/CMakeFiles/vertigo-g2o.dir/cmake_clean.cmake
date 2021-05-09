1f9ea42ee0c8b21da90f4fa7a687f6a3128e90ed
Frozenheart1998 <409106496@qq.com>
2021-05-09 14:02:41 +0800

    First Commit.

---
+++ trunk/build/src/g2o/CMakeFiles/vertigo-g2o.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../../../lib/libvertigo-g2o.pdb"
+  "../../../lib/libvertigo-g2o.so"
+  "CMakeFiles/vertigo-g2o.dir/edge_se2Switchable.cpp.o"
+  "CMakeFiles/vertigo-g2o.dir/edge_switchPrior.cpp.o"
+  "CMakeFiles/vertigo-g2o.dir/types_g2o_robust.cpp.o"
+  "CMakeFiles/vertigo-g2o.dir/vertex_switchLinear.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vertigo-g2o.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
